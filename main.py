5b8df1cb422112d230cb907458b7b4b14f7af223
CAI-KAIYUEKAIYU <98534144+Caikaiyue@users.noreply.github.com>
2022-03-23 01:09:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pymongo
+
+#including names, and password
+uri = "mongodb://Group_4:ESSD8GMd3nLct74@cluster0-shard-00-00.qbwtc.mongodb.net:27017,cluster0-shard-00-01.qbwtc.mongodb.net:27017,cluster0-shard-00-02.qbwtc.mongodb.net:27017/mGroup_4?ssl=true&replicaSet=atlas-u0fo68-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(uri)   #pass in the uri
+
+db = client['Student_information']
+coll1 = db["student_names_collection"]
+
+student1 = {'student_name': 'Student1', 'year_enrolled': 2020}
+result = coll1.insert_one(student1)
+client.close()
+
