dd552cc1e974af151606425e97bbeec2a6871ff6
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-04-19 13:21:29 +0000

    Changes based on latest discussion on 19042022

--- main.py
+++ main.py
@@ -1,58 +1,44 @@
 from flask import Flask, render_template, request
-
+from storage import Student, Club, Activity
 
 
 app = Flask(__name__)
 
 
 
+student_coll = Student()
+cca_coll = Club()
+activity_coll = Activity()
+
+
+
 @app.route('/', methods=["GET"])
-def splash():
-    pass
-
-@app.route('/index', methods=["GET"])
 def index():
-    # link to cca
-    # link to events
+    #Index page shows two boxes, activity and club which directs to /activity and /club
+    #No arg required
+    
+    return render_template('index.html')
     pass
 
-@app.route('/event', methods=["GET", "POST"])
-def event():
-    # list of all events
-    # REQUEST ARGS
-    # when clicked, goes to /event?event=<event name>
-    # indiv event page has
-    # - link to event_add_participant, goes to /event/add_partipant?event=<event_id>
-    # - link to event_remove_participant
-    pass
+@app.route('/activity', methods=['GET'])
+    # Show all activities
 
-@app.route('/event/add_participant', methods=["GET", "POST"])
-def event_add_participant():
-    # Required request arg: event=<event_id>
-    # display list of students who are not participants
-    # When form submitted, send POST request to /event
-    # with event_id and participant_id
-    pass
+    return render_template('activity.html', activity_list = activity_coll.all_activity())
 
-@app.route('/event/remove_participant', methods=["GET", "POST"])
-def event_remove_participant():
-    pass
+    # /activity?id=?  --> Show details of activity, & participants
+    # add participant: page with dropdown list of students
+        # POST /activity/add_participant form [activity_id, student_id]
 
-@app.route('/cca', methods=["GET"])
-def cca():
-    # list of all cca
-    # when clicked, passes name=<cca name> as request arg
-    # each cca has
-    # - link to cca_add_member
-    # - link to cca_remove_member
-    pass
+@app.route('/acitivity/add_participant', methods=['GET', 'POST'])
+def activity_add_participant():
+    student_list = activity_coll.
+    
+@app.route('/club', methods=['GET'])
+    # show all clubs
+    # /club?id=<id>  --> show 1 club
+    # add member: page with dropdown list of students
+        # POST /club/add_member form [club_id, student_id]
 
-@app.route('/cca/add_member', methods=["GET", "POST"])
-def cca_add_member():
-    pass
 
-@app.route('/cca/remove_member', methods=["GET", "POST"])
-def cca_remove_member():
-    pass
 
 app.run('0.0.0.0')
