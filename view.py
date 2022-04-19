614ade1fd89497d7e35e0445b38ef8d8f174f910
JS Ng <45561895+ngjunsiang@users.noreply.github.com>
2022-04-19 13:35:45 +0000

    Add view module (for frontend rendering)

---
+++ view.py
@@ -0,0 +1,29 @@
+from flask import render_template
+
+
+
+def all_clubs():
+    # return html result with list of all clubs
+    pass
+
+def club_with_id(id: int):
+    # return html result with details and members of club id
+    # display add member button
+    pass
+
+def add_club_member(club_id: int):
+    # return html result with dropdown list showing students
+    pass
+
+def all_activities():
+    # return html result with list of all activities
+    pass
+
+def activity_with_id(id: int):
+    # return html result with details and participants of activity id
+    # display add participant button
+    pass
+
+def add_activity_participant(activity_id: int):
+    # return html result with dropdown list showing students
+    pass
