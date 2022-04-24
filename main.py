75aeb37278741101d98485c9a4188f384c879190
CAI KAIYUE <98534144+Caikaiyue@users.noreply.github.com>
2022-04-24 13:16:36 +0000

    1.change main

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def index():
     return render_template('index.html')
     pass
 
+#activity
 @app.route('/activity', methods=['GET'])
 def activity():
     # /activity?id=?  --> Show details of activity, & participants
@@ -104,7 +105,7 @@ def activity_remove_participant():
 
         activity_coll.remove_participant(activity_id, participant_id)
 
-
+#club
 @app.route('/club', methods=['GET'])
 def club():
     # show all clubs
@@ -124,6 +125,88 @@ def club_add_member():
     # POST: add member to db
     cca_coll...
     return ...
+
+@app.route('/club', methods=['GET'])
+def club():
+    # /club?id=?  --> Show details of club, & members
+        # POST /club/add_member form [club_id, student_id]
+
+    #If id in args -> view club details
+    if 'club_id' in request.args:
+        id_ = request.args['club_id']
+        
+        # Validate request arg id
+        if not validate.club_id(id_):
+            abort(400)
+            
+        # Retrieve club record
+        club = club_coll.get(id_)
+        
+        # Retrieve member records
+        members = club_coll.get_members(id_)
+        return view.club(club, members)
+        
+    #if no request args, show all clubs
+    else:
+        # retrieve clubs from storage
+        return view.all_club()
+
+@app.route('/club/add_member', methods=['GET', 'POST'])
+def club_add_member():
+    #If the page is GET request -> choose which club
+    if request.method == 'GET':
+        
+        # Handle error if request arg does not have club_id
+        if "club_id" not in request.arg:
+            abort(400)
+            
+        club_id = request.args['club_id']
+        # Validate club_id
+        if not validate.club_id(club_id):
+            abort(400)
+
+        #get all the members from this club
+        members = club_coll.get_members(club_id)
+        return view.add_club_members(club_id, members)
+
+    #if its a POST request -> add members 
+    elif request.method == 'POST':
+        club_id = request.form['club_id']
+        student_id = request.form['student_id']
+
+        #validate both ids
+        if not (
+            validate.club_id(club_id)
+            and validate.members_id(student_id)
+        ):
+            abort(400)
+
+        # add member to db
+        club_coll.add_member(club_id, student_id)
+
+        return view.add_member_success()
+
+
+@app.route('/club/remove_member', methods=['GET', 'POST'])
+def club_remove_member():
+    #GET: show dropdpwn list of members
+    if request.method == "GET":
+
+        #handle error: if club_id not in request.arg
+        if "club_id" not in request.arg:
+            abort(400)
+
+        club_id = request.arg["club_id"]
+        members = club_coll.get_members(id_)
+        return view.club(club, members)
+
+    elif request.method == 'POST':
+        
+        club_id = request.form['club_id']
+        member_id = request.form['member_id']
+
+        club_coll.remove_member(club_id, member_id)
+
 
 
 
