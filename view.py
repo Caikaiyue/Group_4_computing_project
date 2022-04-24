a464c3d0d91e0425bfd356960505c675522b5e26
Kenneth  Chia <98528218+kcyjj@users.noreply.github.com>
2022-04-24 10:41:51 +0000

    made all html pages and updated view functions

--- view.py
+++ view.py
@@ -1,49 +1,53 @@
 from flask import render_template
 
 
-
 def all_clubs(club_list: list):
     # return html result with list of all clubs
     return render_template(
-        "all_clubs.html",
-        clubs = club_list
+        "clubs.html",
+        clubs=club_list
     )
-    pass
 
-def club_with_id(id: int):
+def club_with_id(club: dict, member_list: list):
     # return html result with details and members of club id
     # display add member button
-    pass
+    return render_template(
+        "activity.html",
+        action="",
+        club = club,
+        members=member_list
+    )
 
-def add_club_member(club_id: int):
+def add_club_member(club_id: int, member_list: list):
     # return html result with dropdown list showing students
-    pass
+    return render_template(
+        "add_member.html",
+        action="",
+        members=member_list,
+    )
 
 def all_activities(activity_list: list):
     # return html result with list of all activities
-    return render_template("activity.html", activities=activity_list)
+    return render_template(
+        "activities.html",
+        activities=activity_list
+    )
     
 
-def activity(activity: dict, participants: list):
+def activity_with_id(activity: dict, participant_list: list):
     # return html result with details and participants of activity id
     # display add participant button
     return render_template(
-        ""
+        "activity.html",
+        action="",
+        activity = activity,
+        participants=participant_list
     )
 
 def add_activity_participant(activity_id: int, participant_list: list):
     # return html result with dropdown list showing students
     return render_template(
-        "addstudent.html",
+        "add_participant.html",
         action="",
-        entity="participant",
-        items=participant_list,
-    )
-
-def add_club_member(club_list: list):
-    return render_template(
-        "addstudent.html",
-        action="",
-        entity="member",
-        items=club_list,
+        participants=participant_list,
     )
