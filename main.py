485e083d43626bd80ab30834e26c016e74edccdd
CAI KAIYUE <98534144+Caikaiyue@users.noreply.github.com>
2022-04-25 08:01:43 +0000

    1. update main

--- main.py
+++ main.py
@@ -31,64 +31,63 @@ def activity():
 
 
     #If id in args -> view activity details
-    if 'activity_id' in request.args:
-        activity_id = request.args['activity_id']
+    if 'id' in request.args:
+        activity_id = request.args['id']
+        #how do I store across functions
+
         
         # Validate request arg id
         if not validate.activity_id(activity_id):
             abort(400)
-            
 
         # Retrieve activity record
-        activity = activity_coll.get_activity_details(id_)
+        activity_detail = activity_coll.get_activity_details(activity_id)
         
         # Retrieve participant records
-        participants = activity_coll.get_participants(id_)
-        return view.activity(activity, participants)
+        participants = activity_coll.get_participants(activity_id)
+        return view.activity_with_id(activity_detail, participants) 
         
     #if no request args, show all activities
     else:
         # retrieve activities from storage
-        return view.all_activity()
+        all_activities = activity_coll.all_activities()
+        return view.all_activities(all_activities)
 
 
 @app.route('/activity/add_participant', methods=['GET', 'POST'])
 def activity_add_participant():
     #If the page is GET request -> choose which activity
-    if request.method == 'GET':
-        
-        # Handle error if request arg does not have activity_id
-        if "activity_id" not in request.arg:
-            abort(400)
-            
+    if 'success' not in request.args:
+
         activity_id = request.args['activity_id']
         # Validate activity_id
 
         if not validate.activity_id(activity_id):
             abort(400)
 
-        #get all the participants from this activity
-        participants = activity_coll.get_participants(activity_id)
+        #get all the students that are not participants
+        participants = activity_coll.participants(activity_id)
+        students = student_coll.all_students(participants)
+        
+        return view.add_activity_participant(activity_id, students)
 
-        return view.add_activity_participant(activity_id, participants)
-
-    #if its a POST request -> add participant 
-    elif request.method == 'POST':
-        activity_id = request.form['activity_id']
-        student_id = request.form['student_id']
-
+    else:
+        activity_id = ...
+        student_id = request.form['student'] #an id
+        
         #validate both ids
         if not (
             validate.activity_id(activity_id)
-            and validate.participant_id(student_id)
+            and validate.student_id(student_id)
         ):
             abort(400)
-
+        
         # add participant to db
         activity_coll.add_participant(activity_id, student_id)
 
-        return view.add_participant_success()
-
+        #get the student details from student_id
+        student_detail = student_coll.get_student_detail(student_id)
+        return view.add_participant_participants(student_detail)
 
 
 @app.route('/activity/remove_participant', methods=['GET', 'POST'])
@@ -111,26 +110,7 @@ def activity_remove_participant():
 
         activity_coll.remove_participant(activity_id, participant_id)
 
-#club
-@app.route('/club', methods=['GET'])
-def club():
-    # show all clubs
-    # /club?id=<id>  --> show 1 club
-    # add member: page with dropdown list of students
-        # POST /club/add_member form [club_id, student_id]
-    # If no request args, show all clubs
-    return view.all_clubs()
-    # If id in request args,
-    # show activity details and participants
-    return view.club(club_id)
 
-@app.route('/club/add_member', methods=['GET', 'POST'])
-def club_add_member():
-    # GET: show dropdown list for adding student
-    return view.add_club_member(club_id)
-    # POST: add member to db
-    cca_coll...
-    return ...
 
 @app.route('/club', methods=['GET'])
 def club():
